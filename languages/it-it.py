9b3fca2937858fe4947e6b30f9c0bff8e30ebc4f
Rick Ree <github@rree.fastmail.fm>
2013-05-27 08:57:27 -0500

    initial commit

---
+++ languages/it-it.py
@@ -0,0 +1,8 @@
+# coding: utf8
+{
+'%Y-%m-%d': '%Y-%m-%d',
+'%Y-%m-%d %H:%M:%S': '%Y-%m-%d %H:%M:%S',
+'Hello World': 'Salve Mondo',
+'Sure you want to delete this object?': 'Sure you want to delete this object?',
+'Welcome to web2py': 'Ciao da wek2py',
+}
